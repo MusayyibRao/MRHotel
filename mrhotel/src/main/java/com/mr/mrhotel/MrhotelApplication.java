3a1e8b568143b9ae666f883a49596b882d3d1edf
Musayyib Rao <musarao6396@gmail.com>
2024-09-04 21:45:09 +0530

    first commit

---
+++ mrhotel/src/main/java/com/mr/mrhotel/MrhotelApplication.java
@@ -0,0 +1,13 @@
+package com.mr.mrhotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrhotelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrhotelApplication.class, args);
+	}
+
+}
---
+++ mrhotel/src/test/java/com/mr/mrhotel/MrhotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mr.mrhotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrhotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
