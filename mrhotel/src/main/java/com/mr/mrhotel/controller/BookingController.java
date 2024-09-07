f67daf9bcbf038b3da59f18c92b43ddf333381c9
Musayyib Rao <musarao6396@gmail.com>
2024-09-07 20:00:22 +0530

    changes for image upload

--- mrhotel/src/main/java/com/mr/mrhotel/controller/BookingController.java
+++ mrhotel/src/main/java/com/mr/mrhotel/controller/BookingController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/bookings")
+//@CrossOrigin(origins = "http://localhost:3000")
 public class BookingController {
 
 
--- mrhotel/src/main/java/com/mr/mrhotel/dto/RoomDto.java
+++ mrhotel/src/main/java/com/mr/mrhotel/dto/RoomDto.java
@@ -2,18 +2,37 @@ package com.mr.mrhotel.dto;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.apache.commons.codec.binary.Base64;
 
 import java.math.BigDecimal;
 import java.util.List;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL)
+@NoArgsConstructor
 public class RoomDto {
 
     private Long id;
     private String roomType;
     private BigDecimal roomPrice;
-    private String photoUrl;
+    private String photo;
     private String roomDescription;
     private List<BookingDto> bookings;
+
+
+    public RoomDto(Long id, String roomType, BigDecimal roomPrice) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+    }
+
+    public RoomDto(Long id, String roomType, BigDecimal roomPrice, byte[] photoBytes, String roomDescription, List<BookingDto> bookings) {
+        this.id = id;
+        this.roomType = roomType;
+        this.roomPrice = roomPrice;
+        this.photo = photoBytes != null ? Base64.encodeBase64String(photoBytes):null;
+        this.roomDescription = roomDescription;
+        this.bookings = bookings;
+    }
 }
